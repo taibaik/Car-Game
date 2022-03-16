8f20d76ca9951a435853c940b67f7aff27809f1a
taibaik <101733400+taibaik@users.noreply.github.com>
2022-03-16 22:58:47 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+command = ""
+started = False
+while True:
+    command = input(">").lower()
+    if command == 'start':
+        if started:
+            print("Car has already started!")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car has already stopped!")
+        else:
+            started = True
+            print("Car stopped...")
+    elif command == "help":
+        print('''
+        Start - to start the car
+        Stop - to stop the car
+        Quit - to quit
+        ''')
+    elif command == "quit":
+        break
+    else:
+        print("Sorry, I don't understand that... ")
